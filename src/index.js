6e137d35509b3b6d657352761e44ed1d44450c31
bertrandk <b.karerangabo@gmail.com>
2015-12-25 14:30:31 -0500

    feat: support redux-simple-router

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-export default function segmentMiddleware({ dispatch, getState }) {
-  return next => action => null;
+export function createTracker() {
+  return ({ dispatch, getState }) =>  next => action => handleAction(next, action);
+}
+
+function handleAction(next: Function, action: Object) {
+  switch (action.type) {
+    case '@@router/INIT_PATH':
+    case '@@router/UPDATE_PATH':
+      window.analytics.page();
+      return next(action);
+    default:
+      return next(action);
+  }
 }
