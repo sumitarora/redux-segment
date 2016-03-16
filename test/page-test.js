676b23799a52ed9aad57e638b55196a8dc953c94
Josh Habdas <jhabdas@gmail.com>
2016-03-16 09:42:03 -0500

    test(page): remove page test for react-router-redux

--- test/page-test.js
+++ test/page-test.js
@@ -3,71 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { compose, createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, Link } from 'react-router';
 import createHistory from 'history/lib/createHashHistory';
 import { syncReduxAndRouter, routeReducer } from 'redux-simple-router';
-import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 import { ReduxRouter, routerStateReducer, reduxReactRouter } from 'redux-router';
 import createAnalyticsStub from './helpers/segment-stub';
 import { createTracker, EventTypes } from '../src/index';
 
 
-test.only('Page - router support', t => {
-  t.test('react-router-redux', st => {
-    st.plan(2);
-
-
-    window.analytics = createAnalyticsStub();
-    const node = document.createElement('div');
-    const tracker = createTracker();
-    const reducer = combineReducers({
-      routing: routerReducer,
-    });
-    const store = compose(
-      applyMiddleware(tracker)
-    )(createStore)(reducer);
-    const Component = () =>
-      <div>
-        <h1>Hello!</h1>
-        <ul>
-          <li><Link to="/foo">Foo</Link></li>
-          <li><Link to="/bar">Bar</Link></li>
-        </ul>
-      </div>;
-    const history = syncHistoryWithStore(hashHistory, store);
-    ReactDOM.render(
-      <Provider store={store}>
-        <div>
-          <Router history={history}>
-            <Route path="/" component={Component}>
-              <IndexRoute component={Component}/>
-              <Route path="foo" component={Component}/>
-              <Route path="bar" component={Component}/>
-            </Route>
-          </Router>
-        </div>
-      </Provider>,
-      node
-    );
-
-
-    const initialEvent = window.analytics[0] && window.analytics[0][0];
-    st.equal(initialEvent, 'page', 'triggers page event on load');
-
-    const fooLink = node.querySelector('a[href$="foo"]');
-    // Wait for route change to propagate to store
-    store.subscribe(() => {
-      const fooLinkEvent = window.analytics[1] && window.analytics[1][0];
-      st.equal(fooLinkEvent, 'page', 'triggers page event on navigation');
-
-      window.analytics = null;
-    });
-    fooLink.click();
-
-
-    ReactDOM.unmountComponentAtNode(node);
-  });
-
+test('Page - router support', t => {
   t.test('redux-simple-router', st => {
     st.plan(2);
 
